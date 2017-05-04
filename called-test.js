947cc9e7981abb65fd77d8f4177912dd4e235db0
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-04 17:10:11 +0800

    Updated test.

--- called-test.js
+++ called-test.js
@@ -1,13 +1,15 @@
-
+const assert = require( "assert" );
 const called = require( "./called.js" );
 
 let test = called( function test( number, string ){
 	return string;
 } );
-console.log( test( 123, "hello world" ) );
+assert.equal( test( 123, "hello world" ), "hello world" );
 
-console.log( test( "yeah", "sample" ) );
+assert.equal( test( "yeah", "sample" ), "hello world" );
 
-console.log( called( function callback( error, result ){
+assert.equal( called( function callback( error, result ){
 	//do nothing
-}, true )( null, "hello yeah" ) );
+}, true )( null, "hello yeah" ), "hello yeah" );
+
+console.log( "ok" );
