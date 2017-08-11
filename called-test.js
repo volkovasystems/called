fcfd417dd30b87410d67659fe972f5c1dfb7964c
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-11 16:28:17 +0800

    Updated test.

--- called-test.js
+++ called-test.js
@@ -6,8 +6,8 @@ let test = called( function test( number, string ){
 	return string;
 } );
 
-assert.equal( test( 123, "hello world" ), "hello world", "should return 'hello world'" );
+assert.equal( test( 123, "hello world" ), "hello world", "should be equal to 'hello world'" );
 
-assert.equal( test( "yeah", "sample" ), "hello world", "should return 'hello world'" );
+assert.equal( test( "yeah", "sample" ), "hello world", "should be equal to 'hello world'" );
 
 console.log( "ok" );
